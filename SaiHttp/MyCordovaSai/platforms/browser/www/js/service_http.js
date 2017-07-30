41aad48a0729c8410a60cbec28cc85fa6793697b
Shruti Nallari <shruti.nallari@gmail.com>
2017-07-29 20:30:05 -0400

    with .gitignore

--- SaiHttp/MyCordovaSai/platforms/browser/www/js/service_http.js
+++ SaiHttp/MyCordovaSai/platforms/browser/www/js/service_http.js
@@ -1,32 +1,34 @@
-angular.module('myApp',[]).controller('mycontroller',['$scope','$http',function($scope,$http){
-	$scope.storeitems={};
-	$scope.kitchenitems={};
-	$scope.status="";
-	$scope.resetStore=function(){
-		$scope.status=";"
-		$http.get('/reset/data').success(function(data,status,headers,config){
-		$scope.storeitems=data;
-	}).error(function(data,status,headers,config){
-		$scope.status=data;
-	});
-	};
-	$scope.buyItem=function(buyItem){
-		$http.post('/buy/item',{item:buyItem}).success(function(data,status,headers,config){
-			$scope.storeitems=data;
-			if($scope.kitchenitems.hasOwnProperty(buyItem)){
-				$scope.kitchenitems[buyItem]+=1;
-			}else{
-				$scope.kitchenitems[buyItem]=1;
-				$scope.status="Purchased"+buyItem;
-			}
-		}).error(function(data,status,headers,config){
-			$scope.status=data.msg;
+angular.module('myApp', []).controller('mycontroller', [ '$scope', '$http', function($scope, $http) {
+	$scope.storeitems = {};
+	$scope.kitchenitems = {};
+	$scope.status = "";
+	$scope.resetStore = function() {
+		$scope.status = ";"
+		$http.get('/reset/data').success(function(data, status, headers, config) {
+			$scope.storeitems = data;
+		}).error(function(data, status, headers, config) {
+			$scope.status = data;
 		});
 	};
-	$scope.useItem=function(useItem){
-		if($scope.kitchenitems[useItem]>0){
-			$scope.kitchenitems[useItem]-=1;
+	$scope.buyItem = function(buyItem) {
+		$http.post('/buy/item', {
+			item : buyItem
+		}).success(function(data, status, headers, config) {
+			$scope.storeitems = data;
+			if ($scope.kitchenitems.hasOwnProperty(buyItem)) {
+				$scope.kitchenitems[buyItem] += 1;
+			} else {
+				$scope.kitchenitems[buyItem] = 1;
+				$scope.status = "Purchased" + buyItem;
+			}
+		}).error(function(data, status, headers, config) {
+			$scope.status = data.msg;
+		});
+	};
+	$scope.useItem = function(useItem) {
+		if ($scope.kitchenitems[useItem] > 0) {
+			$scope.kitchenitems[useItem] -= 1;
 		}
 	};
-	
-}]);
+
+} ]);
